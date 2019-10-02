6b96b5c378c543c56e92a6c780fc014d0aeaeb41
Vishnu Jeyarathnam <vjeyarathnam@spartaglobal.com>
2019-10-02 10:52:43 +0100

    created for and while loops in pyodbc

--- db_connect.py
+++ db_connect.py
@@ -7,3 +7,44 @@ password = 'Passw0rd2018'
 # Establish a connection
 conn_nwdb = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+password)
 print(conn_nwdb)
+
+#Create a cursor
+#allows us to execute read only queries on the db
+cursor=conn_nwdb.cursor()
+cursor.execute("SELECT * FROM Customers")
+
+#Fetch rows from cursor - .fetchone()   #it maintains state
+# row = cursor.fetchone()
+# print(row)
+#
+# row = cursor.fetchone() #it maintains state
+# print(row.ContactName)
+#
+# row = cursor.fetchone() #it maintains state
+# print(row.ContactName)
+
+# .fetchall()
+#This is bad
+
+
+rows = cursor.execute("SELECT *FROM Customers").fetchall()
+print(rows)
+#print(len(rows)) #if this is a list then we can iterate
+
+rows = cursor.execute("SELECT * FROM Products").fetchall()
+#we can iterate
+for record in rows:
+    print(type(record))
+    print(record.UnitPrice) #we can access the column of a specific record
+
+    # however, this is dangerous as we said.
+    # because we can clog our machine with too much data
+    #we can use while loop to be more efficient
+
+rows = cursor.execute("SELECT* FROM Products")
+
+while True:
+    record= rows.fetchone()
+    if record is None:
+        break
+    print(record.UnitPrice)
